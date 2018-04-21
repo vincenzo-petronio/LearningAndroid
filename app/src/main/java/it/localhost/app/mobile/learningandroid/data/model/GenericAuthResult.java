5ba25c67c7b98248fd12ef35e0aea4796d2a220c
vincenzo-petronio <vincenzo.petronio@gmail.com>
2018-04-21 18:28:53 +0200

    FIX

--- app/src/main/java/it/localhost/app/mobile/learningandroid/data/model/GenericAuthResult.java
+++ app/src/main/java/it/localhost/app/mobile/learningandroid/data/model/GenericAuthResult.java
@@ -1,17 +1,22 @@
 package it.localhost.app.mobile.learningandroid.data.model;
 
 /**
+ * Generic Version of the GenericAuthResult class. <br>
+ * A type variable T can be any non-primitive type: class type, interface type, array type, or even another type variable.
+ *
  * @author vincenzo.petronio on 12/04/2018.
  */
 public class GenericAuthResult<T> {
 
     private final T mGenericResult;
 
+    // Item 4: Enforce noninstantiability with a private constructor
     private GenericAuthResult(T t) {
         this.mGenericResult = t;
     }
 
-    public static <T2> GenericAuthResult<T2> newGenericAuthResult(T2 t2) {
+    // Item 1: consider static factory methods instead of constructors
+    public static <T2> GenericAuthResult newGenericAuthResult(T2 t2) {
         return new GenericAuthResult<T2>(t2);
     }
 
--- app/src/main/java/it/localhost/app/mobile/learningandroid/helper/login/ILoginStrategy.java
+++ app/src/main/java/it/localhost/app/mobile/learningandroid/helper/login/ILoginStrategy.java
@@ -1,16 +1,17 @@
 package it.localhost.app.mobile.learningandroid.helper.login;
 
 import bolts.Task;
+import it.localhost.app.mobile.learningandroid.data.model.GenericAuthResult;
 
 /**
  * @author vincenzo.petronio on 20/02/2018.
  */
 
-public interface ILoginStrategy<T> {
+public interface ILoginStrategy {
 
     /**
      * @param username String
      * @param psw      String
      */
-    Task<T> login(String username, String psw) throws Exception;
+    Task<GenericAuthResult> login(String username, String psw) throws Exception;
 }
