f8d965c25df0cda1e3acee96687235dfcf05cf2b
vincenzo-petronio <vincenzo.petronio@gmail.com>
2018-05-01 10:11:03 +0200

    CHG refactor to generics

--- app/src/main/java/it/localhost/app/mobile/learningandroid/ui/activity/MainActivity.java
+++ app/src/main/java/it/localhost/app/mobile/learningandroid/ui/activity/MainActivity.java
@@ -3,6 +3,7 @@ package it.localhost.app.mobile.learningandroid.ui.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ArrayAdapter;
@@ -40,6 +41,103 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
+    @OnItemClick(R.id.lvItems)
+    void onItemClick(int position) {
+        Log.d(TAG, "OnItemSelected: " + lvItems.getItemAtPosition(position));
+
+        Class<? extends AppCompatActivity> activity = null;
+
+        switch (position) {
+            case 0:
+                activity = JsoupActivity.class;
+                break;
+            case 1:
+                // RX
+                activity = RxActivity.class;
+                break;
+            case 2:
+                break;
+            case 3:
+                activity = NestedScrollActivity.class;
+                break;
+            case 4:
+                activity = RecyclerViewActivity.class;
+                break;
+            case 5:
+                activity = PercentActivity.class;
+                break;
+            case 6:
+                activity = CollapsingActivity.class;
+                break;
+            case 7:
+                // RETROFIT
+                activity = RetrofitActivity.class;
+                break;
+            case 8:
+                activity = AnnotationActivity.class;
+                break;
+            case 9:
+                //REALM
+                activity = RealmActivity.class;
+                break;
+            case 10:
+                activity = DifferentRowActivity.class;
+                break;
+            case 11:
+                // REFLECTION
+                activity = ReflectionActivity.class;
+                break;
+            case 12:
+                // CUSTOMVIEW
+                activity = CustomViewActivity.class;
+                break;
+            case 13:
+                // XML BINDING
+                activity = XMLBindingActivity.class;
+                break;
+            case 14:
+                // WIRE
+                break;
+            case 15:
+                // CONSTRAINT
+                activity = ConstraintActivity.class;
+                break;
+            case 16:
+                // PARCELER
+                break;
+            case 17:
+                // EVENTBUS
+                activity = PubSubActivity.class;
+                break;
+            case 18:
+                // LITHO
+                activity = LithoActivity.class;
+                break;
+            case 19:
+                // DAGGER
+                activity = DaggerActivity.class;
+                break;
+            case 20:
+                activity = ServerSyncActivity.class;
+                break;
+            case 21:
+                // CONCURRENCY
+                activity = ConcurrencyActivity.class;
+                break;
+            case 22:
+                // PATTERNS
+                activity = PatternsActivity.class;
+                break;
+            //...
+            default:
+                break;
+        }
+
+        if (activity != null) {
+            navigateTo(activity);
+        }
+    }
+
     private void initUi() {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
@@ -51,116 +149,11 @@ public class MainActivity extends AppCompatActivity {
         lvItems.setAdapter(arrayAdapter);
     }
 
-    @OnItemClick(R.id.lvItems)
-    void onItemClick(int position) {
-        Log.d(TAG, "OnItemSelected: " + lvItems.getItemAtPosition(position));
-
-        Intent i;
-
-        switch (position) {
-            case 0:
-                i = new Intent(MainActivity.this, JsoupActivity.class);
-                startActivity(i);
-                break;
-            case 1:
-                // RX
-                i = new Intent(MainActivity.this, RxActivity.class);
-                startActivity(i);
-                break;
-            case 2:
-                break;
-            case 3:
-                i = new Intent(MainActivity.this, NestedScrollActivity.class);
-                startActivity(i);
-                break;
-            case 4:
-                i = new Intent(MainActivity.this, RecyclerViewActivity.class);
-                startActivity(i);
-                break;
-            case 5:
-                i = new Intent(MainActivity.this, PercentActivity.class);
-                startActivity(i);
-                break;
-            case 6:
-                i = new Intent(MainActivity.this, CollapsingActivity.class);
-                startActivity(i);
-                break;
-            case 7:
-                // RETROFIT
-                i = new Intent(MainActivity.this, RetrofitActivity.class);
-                startActivity(i);
-                break;
-            case 8:
-                i = new Intent(MainActivity.this, AnnotationActivity.class);
-                startActivity(i);
-                break;
-            case 9:
-                //REALM
-                i = new Intent(MainActivity.this, RealmActivity.class);
-                startActivity(i);
-                break;
-            case 10:
-                i = new Intent(MainActivity.this, DifferentRowActivity.class);
-                startActivity(i);
-                break;
-            case 11:
-                // REFLECTION
-                i = new Intent(MainActivity.this, ReflectionActivity.class);
-                startActivity(i);
-                break;
-            case 12:
-                // CUSTOMVIEW
-                i = new Intent(MainActivity.this, CustomViewActivity.class);
-                startActivity(i);
-                break;
-            case 13:
-                // XML BINDING
-                i = new Intent(MainActivity.this, XMLBindingActivity.class);
-                startActivity(i);
-                break;
-            case 14:
-                // WIRE
-                break;
-            case 15:
-                // CONSTRAINT
-                i = new Intent(MainActivity.this, ConstraintActivity.class);
-                startActivity(i);
-                break;
-            case 16:
-                // PARCELER
-                break;
-            case 17:
-                // EVENTBUS
-                i = new Intent(MainActivity.this, PubSubActivity.class);
-                startActivity(i);
-                break;
-            case 18:
-                // LITHO
-                i = new Intent(MainActivity.this, LithoActivity.class);
-                startActivity(i);
-                break;
-            case 19:
-                // DAGGER
-                i = new Intent(MainActivity.this, DaggerActivity.class);
-                startActivity(i);
-                break;
-            case 20:
-                i = new Intent(MainActivity.this, ServerSyncActivity.class);
-                startActivity(i);
-                break;
-            case 21:
-                // CONCURRENCY
-                i = new Intent(MainActivity.this, ConcurrencyActivity.class);
-                startActivity(i);
-                break;
-            case 22:
-                // PATTERNS
-                i = new Intent(MainActivity.this, PatternsActivity.class);
-                startActivity(i);
-                break;
-            //...
-            default:
-                break;
-        }
+    /**
+     * @param activity AppCompatActivity
+     */
+    private void navigateTo(@NonNull Class<? extends AppCompatActivity> activity) {
+        Intent i = new Intent(MainActivity.this, activity);
+        startActivity(i);
     }
 }
