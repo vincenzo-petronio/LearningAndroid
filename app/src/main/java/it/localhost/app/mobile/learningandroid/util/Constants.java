f7cafedd8252126ce67074d79ecea3faac692ce9
vincenzo-petronio <vincenzo.petronio@gmail.com>
2018-06-07 15:55:31 +0200

    ADD network security configuration

--- app/src/main/java/it/localhost/app/mobile/learningandroid/util/Constants.java
+++ app/src/main/java/it/localhost/app/mobile/learningandroid/util/Constants.java
@@ -6,7 +6,7 @@ package it.localhost.app.mobile.learningandroid.util;
 
 public class Constants {
 
-    public static final String URL_JSOUP = "http://www.italotreno.it/";
+    public static final String URL_JSOUP = "https://www.italotreno.it/";
     public static final String PATH_JSON_TODOS = "todos.json";
 
     // API
